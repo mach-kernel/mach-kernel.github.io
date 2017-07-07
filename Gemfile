4c9c398d7cf79ef3b081135c2024f9088eea006a
David Stancu <dstancu@nyu.edu>
2017-07-07 10:57:27 -0400

    working asset pipeline + autowatch, working on the actual site now

--- Gemfile
+++ Gemfile
@@ -16,10 +16,13 @@ gem "jekyll", "3.5.0"
 
 # If you have any plugins, put them here!
 group :jekyll_plugins do
-   gem "jekyll-feed", "~> 0.6"
-   gem 'jekyll-assets'
+  gem "jekyll-feed", "~> 0.6"
+  gem 'jekyll-assets'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+group :development do
+  gem 'foreman'
+end
--- _assets/webpack_entry.js
+++ _assets/webpack_entry.js
@@ -1,1 +1,26 @@
+require('jquery');
 require('bootstrap-loader');
+
+import * as heros from 'hero-patterns';
+
+// TODO: awful
+var randomHero = function() {
+  var heros = Object.values(this);
+
+  var rgb = $('#contrast').css('background-color')
+                          .split('(')[1]
+                          .split(')')[0]
+                          .split(',');
+
+  var hex = rgb.map(function(a) {
+    return (parseInt(a)).toString(16);
+  });
+
+  document.body
+          .style
+          .backgroundImage = heros[Math.floor(Math.random() * (heros.length - 1))](
+    '#' + hex.join(''), 0.50
+  );
+}.bind(heros);
+
+$(document).ready(randomHero);
